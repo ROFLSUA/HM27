cb1f7b6a3f002ab9eec4cd1b816a1b46680d956d
Dania <135432914+ROFLSUA@users.noreply.github.com>
2023-10-10 16:19:25 +0300

    Add files via upload

---
+++ Container.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Container() {
+  return (
+    <div className="container">
+      <h2>Зміст</h2>
+      <p>Тут буде вміст про спорт</p>
+    </div>
+  );
+}
+
+export default Container;
